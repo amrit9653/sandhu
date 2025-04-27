7954a31d4446c8cc080cd54f7433c153c4b8eed6
amrit9653 <amritsinghamritsingh036@gmail.com>
2025-04-28 00:26:32 +0530

    Add files via upload

---
+++ Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div>
+    <div className='h-[70px] w-[70px]  bg-black text-white'>
+
+    </div>
+    
+    
+    </div>
+  )
+}
